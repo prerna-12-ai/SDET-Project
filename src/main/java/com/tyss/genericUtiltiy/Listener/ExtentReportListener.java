a49640ab118b3511c4588d927c6ec3362264fe0c
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2023-03-09 13:08:44 +0530

    Update ExtentReportListener.java

--- src/main/java/com/tyss/genericUtiltiy/Listener/ExtentReportListener.java
+++ src/main/java/com/tyss/genericUtiltiy/Listener/ExtentReportListener.java
@@ -21,6 +21,7 @@ import com.tyss.genericUtiltiy.annotation.Report;
 public class ExtentReportListener implements ITestListener, ISuiteListener,IClassListener {
 //modification 1
 	//modification 2
+	//modification 3
 	private ReportUtility report;
 	public static ReportUtility sreport;
 
